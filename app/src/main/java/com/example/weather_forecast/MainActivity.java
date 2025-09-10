96a570d4da03ba0166ff5e63870fd7fcdf538e9d
Maksonik111 <maxjukoww@gmail.com>
2025-09-10 22:36:00 +0300

    second commit

--- app/src/main/java/com/example/weather_forecast/MainActivity.java
+++ app/src/main/java/com/example/weather_forecast/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.weather_forecast;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView display;
+    Button main_button;
+    EditText enter_city;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 }
