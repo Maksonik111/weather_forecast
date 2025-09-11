ac7e0d76bb91ed035bd80fd657e052df3ab1cdf8
Maksonik111 <maxjukoww@gmail.com>
2025-09-11 21:44:16 +0300

    logic program

--- app/src/main/java/com/example/weather_forecast/MainActivity.java
+++ app/src/main/java/com/example/weather_forecast/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.weather_forecast;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,5 +31,32 @@ public class MainActivity extends AppCompatActivity {
             return insets;
 
         });
+        display = findViewById(R.id.display);
+        main_button = findViewById(R.id.main_button);
+        enter_city = findViewById(R.id.enter_city);
+
+        main_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(enter_city.getText().toString().trim().isEmpty()){
+                    Toast.makeText(MainActivity.this, R.string.warning_text, Toast.LENGTH_LONG).show();
+                }
+                else{
+                    String city = enter_city.getText().toString();
+                    String key = "fc20c4f7067949bb6081669a9d0e917c";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric&lang=ru";
+
+                    new GetURLData().execute();
+                }
+            }
+        });
+    }
+
+    private class GetURLData extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return "";
+        }
     }
 }
