e732318488708f745b32c980e2757c0c05f5fd1a
Maksonik111 <maxjukoww@gmail.com>
2025-09-14 23:54:57 +0300

    logic program 2.0

--- app/src/main/java/com/example/weather_forecast/MainActivity.java
+++ app/src/main/java/com/example/weather_forecast/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weather_forecast;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -14,9 +15,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView display;
+    TextView display2;
+
     Button main_button;
     EditText enter_city;
 
@@ -32,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         });
         display = findViewById(R.id.display);
+        display2 = findViewById(R.id.display2);
         main_button = findViewById(R.id.main_button);
         enter_city = findViewById(R.id.enter_city);
 
@@ -46,17 +62,85 @@ public class MainActivity extends AppCompatActivity {
                     String key = "fc20c4f7067949bb6081669a9d0e917c";
                     String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric&lang=ru";
 
-                    new GetURLData().execute();
+                    new GetURLData().execute(url);
                 }
             }
         });
     }
 
-    private class GetURLData extends AsyncTask<String, String, String>{
+    private class GetURLData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            display.setText("Обработка...");
+
+        }
 
         @Override
         protected String doInBackground(String... strings) {
-            return "";
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+
+                }
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return null;
+
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                JSONObject obj = new JSONObject(result);
+
+                display.setText("Температура:" + obj.getJSONObject("main").getDouble("temp"));
+
+                JSONArray weatherArray = obj.getJSONArray("weather");
+                if(weatherArray.length() > 0 ){
+                    JSONObject firstWeather = weatherArray.getJSONObject(0);
+                    String description = firstWeather.getString("description");
+                    display2.setText(description);
+
+                }
+
+                display2.setText(obj.getJSONObject("weather").getString("main"));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
